4d77d9f684a3fec95c11b2a063b8aad69de10d9e
kevin.santos <kevinsantos12111@gmail.com>
2021-09-14 21:03:53 -0300

    Teste componentes

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,69 @@
 import {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 function Home(){
 
+    const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div>
-            <div>Home 8</div>
-            <Contador/>
+        <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+                Teste
+            </Typography>
+            <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+
+        <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+
         </div>
-        )
-
-}
-
-function Contador(){
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-
+    );
 }
 
 export default Home
